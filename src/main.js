a8f0510181873d5dfb8d0ba3c61f0dcf2c51ee03
Sunny Wang <sunnywen125@gmail.com>
2020-04-10 23:24:15 -0400

    fix_dependencies

--- src/main.js
+++ src/main.js
@@ -4,12 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
-import 'babel-polyfill'
 import store from './store'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
-import 'swiper/css/swiper.css'
+require('swiper/dist/css/swiper.css')
 
 Vue.config.productionTip = false
 Vue.use(VueAwesomeSwiper)
